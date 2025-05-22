5778c6e8ee9f12386cfce929ccf248b8e07c8034
Kemal Ardhika <m.kemal.ardhika@gmail.com>
2025-05-22 17:11:11 +0700

    Penambahan Fungsi Many to One Product(Cloudinary)

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -4,7 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+class Product extends Model
+{
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+}
+
 class ProductImage extends Model
 {
-    //
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
