5d0fa5c09f9aa8ce26ddd4437edf2e5d1f31624b
Kemal Ardhika <m.kemal.ardhika@gmail.com>
2025-05-22 14:17:04 +0700

    Penambahan model ProductImage(model,migration,config) untuk integrasi Cloudinary

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,9 +1,7 @@
-// app/Models/Product.php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model {
-    // Daftar kolom yang boleh diisi massal
     protected $fillable = ['name', 'description', 'price'];
 
     // Relasi: satu produk punya banyak CartItem
