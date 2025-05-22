cecb8aae8ddc40412ad2932c6cc3db650c556a53
Kemal Ardhika <m.kemal.ardhika@gmail.com>
2025-05-22 19:39:06 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('products', ProductController::class);
+Route::get('cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('cart', [CartController::class, 'store'])->name('cart.store');
+Route::put('cart/{cartItem}', [CartController::class, 'update'])->name('cart.update');
+Route::delete('cart/{cartItem}', [CartController::class, 'destroy'])->name('cart.destroy');
